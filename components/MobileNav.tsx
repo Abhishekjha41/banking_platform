4aee1fd264391d303767c956f873ca501ea72045
Abhishek Jha <abhishekjha6908@gmail.com>
2024-10-27 23:54:36 +0530

    Implement auth, appwrite and sentry

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -14,6 +14,7 @@ import {
     SheetTitle,
     SheetTrigger,
   } from "@/components/ui/sheet"
+import Footer from './Footer';
 const MobileNav = ({ user }:MobileNavProps) => {
     const pathname = usePathname();
   return (
@@ -30,8 +31,8 @@ const MobileNav = ({ user }:MobileNavProps) => {
         </SheetTrigger>
         <SheetContent side="left" className='border-none bg-white'>
             <Link href="/" className="cursor-pointer items-center gap-1 px-4 flex">
-            <Image src="/icons/logo.svg" width={34} height={34} alt='Bankify Logo'/>
-            <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>Bankify</h1>
+                <Image src="/icons/logo.svg" width={34} height={34} alt='Bankify Logo'/>
+                <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>Bankify</h1>
             </Link>
             <div className='mobileb=nav-sheet'>
                 <SheetClose asChild>
@@ -63,7 +64,7 @@ const MobileNav = ({ user }:MobileNavProps) => {
                     </nav>
                 </SheetClose>
 
-                FOOTER
+                <Footer user={user} type="mobile"/>
             </div>
             
         </SheetContent>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import { sidebarLinks } from '@/constants'
 import { usePathname } from 'next/navigation'
+import Footer from './Footer';
 const Sidebar = ({ user }:SiderbarProps) => {
     const pathname = usePathname();
 
@@ -36,7 +37,8 @@ const Sidebar = ({ user }:SiderbarProps) => {
            })}
            USER
         </nav>
-        FOOTER
+        
+        <Footer user={user} />
     </section>
   )
 }
